43de7de1cfe84e3a82fdb4fd936da1048d553e72
bmseibert <bmseibert@wpi.edu>
2020-01-19 10:45:34 -0500

    Fixed bug with recursion in ex2

--- ex2.py
+++ ex2.py
@@ -6,30 +6,17 @@ def isreverse(s1, s2):
     s1List = list(s1)
     s2List = list(s2)
     done = False
+    result = False
     if s1Length == s2Length:
         # Case 2: If parameters are the same 1 letter string, or nothing return true
         if s1Length == 0 or (s1Length == 1 and s1 == s2):
             done = True
-            print(True)
-            return True
+            result = True
+            #return True
         # Case 3: Check if external letters are the same, then pop them and loop
         elif s1List[0] == s2List[len(s2List)-1] and done is not True:
             s1List.pop(0)
             s2List.pop(len(s2List)-1)
             isreverse(s1List, s2List)
-        else:
-            # print("False")
-            return False
-    else:
-        # print("False")
-        return False
-
-
-
-
-# isreverse("a", "a")
-# isreverse("", "")
-# isreverse("ab", "ba")
-# isreverse("abc", "cba")
-# isreverse("abcd", "bfeg")
-# isreverse("fer", "a")
+            result = True
+    return result
